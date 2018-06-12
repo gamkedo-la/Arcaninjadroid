471f7b32e20c5de4e77f3d2af1049a38555b286c
JackSnakeMGS4 <jaimerivas81@yahoo.com>
2018-06-12 13:35:27 -0700

    XP system still WIP

--- js/UnusedCode/XP.js
+++ js/UnusedCode/XP.js
@@ -3,12 +3,12 @@ function XPclass()
 {
 	/*all characters will start off at lvl 1.
 		will hopefully feature the following: 
-	 	gaining XP depending on either the type of enemy defeated and that enemy lvl or both.
+	 	gaining XP depending on either the type of enemy defeated or that enemy lvl or both.
 		higher lvl enemies provide more xp while lower lvl enemies provide minimal xp
 		or this could also go based off of the player's combo
 		or both
 	*/
-	this.lvl = 1;
+	this.currentLVL = 1;
 	this.nextLVL = this.lvl + 1;
 
 	this.comboMultiplier = 1.0;
@@ -16,47 +16,58 @@ function XPclass()
 
 	this.xpDrop;
 
-	this.startingXP = 0.0;
-	this.currentXP = this.calculateCurrentXP(this.lvl, this.multiplier);
+	this.currentXP = this.calculateCurrentXP(this.currentLVL, this.multiplier);
 	this.xpForNextLVL = this.calculateNextXP(this.nextLVL, this.multiplier);
 	this.diff = this.xpForNextLVL - this.currentXP;
 
 	this.getCurrentLVL = function()
 	{
-		return this.lvl;
+		return this.currentLVL;
 	}
 
 	this.setLVL = function()
 	{
-
+		if(this.diff == 0)
+		{
+			this.currentLVL = this.nextLVL;
+		}
 	}
 
-	this.setXPdrop = function(characterType)
+	this.calculateXPdrop = function(LVL, )
 	{
-		switch(characterType)
-		{
-			case "enemy 1":
-				break;
-
-			default:
-				break;
-		}
-
-		switch(this.lvl)
+		switch(LVL)
 		{
 			case 1:
 				break;
 		}
 	}
 
-	this.getXP = function()
+	this.getXPdrop = function()
 	{
+		calculateXPdrop();
+		return this.xpDrop;
+	}
 
+	this.getCurrentXP = function()
+	{
+		return this.currentXP;
+	}
+
+	this.getXPtoNextLevel = function()
+	{
+		return this.xpForNextLVL;
 	}
 
 	this.calculateCurrentXP = function(lvl, multiplier)
 	{
-
+		if(lvl == 1)
+		{
+			return 0;
+		}
+		else
+		{
+			return (lvl * (lvl + 1)) * multiplier;
+		}	
 	}
 
 	this.calculateNextXP = function(lvl, multiplier)
@@ -64,7 +75,7 @@ function XPclass()
 		return (lvl * (lvl + 1)) * multiplier;
 	}
 
-	this.setComboMultiplier = function()
+	this.setComboMultiplier = function(currentCombo)
 	{
 
 	}
