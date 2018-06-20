50d5618e3906712da90608fcfd702a117def453a
OmegaLarmor <remy-lapointe@hotmail.ca>
2018-06-19 22:43:09 -0400

    Added demo outline to player. Removed some placeholder files.

--- CodedAssets/AnimationData/playerIdle.js
+++ CodedAssets/AnimationData/playerIdle.js
@@ -1,83 +1,20 @@
 var playerIdleData = { "frames": [
    {
-    "filename": "arcaninjadroid #Shine+Magic 0.aseprite",
+    "filename": "arcaninjadroid #Fist.aseprite",
     "frame": { "x": 0, "y": 0, "w": 32, "h": 32 },
     "rotated": false,
     "trimmed": false,
     "spriteSourceSize": { "x": 0, "y": 0, "w": 32, "h": 32 },
     "sourceSize": { "w": 32, "h": 32 },
     "duration": 1500
-   },
-   {
-    "filename": "arcaninjadroid #Shine+Magic 1.aseprite",
-    "frame": { "x": 32, "y": 0, "w": 32, "h": 32 },
-    "rotated": false,
-    "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 32, "h": 32 },
-    "sourceSize": { "w": 32, "h": 32 },
-    "duration": 120
-   },
-   {
-    "filename": "arcaninjadroid #Shine+Magic 2.aseprite",
-    "frame": { "x": 64, "y": 0, "w": 32, "h": 32 },
-    "rotated": false,
-    "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 32, "h": 32 },
-    "sourceSize": { "w": 32, "h": 32 },
-    "duration": 80
-   },
-   {
-    "filename": "arcaninjadroid #Shine+Magic 3.aseprite",
-    "frame": { "x": 96, "y": 0, "w": 32, "h": 32 },
-    "rotated": false,
-    "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 32, "h": 32 },
-    "sourceSize": { "w": 32, "h": 32 },
-    "duration": 80
-   },
-   {
-    "filename": "arcaninjadroid #Shine+Magic 4.aseprite",
-    "frame": { "x": 128, "y": 0, "w": 32, "h": 32 },
-    "rotated": false,
-    "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 32, "h": 32 },
-    "sourceSize": { "w": 32, "h": 32 },
-    "duration": 60
-   },
-   {
-    "filename": "arcaninjadroid #Shine+Magic 5.aseprite",
-    "frame": { "x": 160, "y": 0, "w": 32, "h": 32 },
-    "rotated": false,
-    "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 32, "h": 32 },
-    "sourceSize": { "w": 32, "h": 32 },
-    "duration": 60
-   },
-   {
-    "filename": "arcaninjadroid #Shine+Magic 6.aseprite",
-    "frame": { "x": 192, "y": 0, "w": 32, "h": 32 },
-    "rotated": false,
-    "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 32, "h": 32 },
-    "sourceSize": { "w": 32, "h": 32 },
-    "duration": 120
-   },
-   {
-    "filename": "arcaninjadroid #Shine+Magic 7.aseprite",
-    "frame": { "x": 224, "y": 0, "w": 32, "h": 32 },
-    "rotated": false,
-    "trimmed": false,
-    "spriteSourceSize": { "x": 0, "y": 0, "w": 32, "h": 32 },
-    "sourceSize": { "w": 32, "h": 32 },
-    "duration": 120
    }
  ],
  "meta": {
   "app": "http://www.aseprite.org/",
   "version": "1.2.8-x64",
-  "image": "C:\\Users\\Remy\\Documents\\Gamkedo\\Arcaninjadroid\\images\\arcaninjadroid.png",
+  "image": "C:\\Users\\Remy\\Documents\\Gamkedo\\Arcaninjadroid\\images\\Characters\\Player\\playerIdle.png",
   "format": "RGBA8888",
-  "size": { "w": 256, "h": 32 },
+  "size": { "w": 32, "h": 32 },
   "scale": "1",
   "slices": [
   ]
