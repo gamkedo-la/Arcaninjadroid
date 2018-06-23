c269057b3d360c80eba693026070b63d8e7dee9d
McFunkypants <breakdance@mcfunkypants.com>
2018-06-22 20:16:04 -0700

    faded out the top and bottom of the woosh trail sfx artwork

--- js/Engine/WooshTrail.js
+++ js/Engine/WooshTrail.js
@@ -9,7 +9,9 @@ function WooshTrail(imageName) {
     let trailXY = []; // list of previous positions
     if (!imageName) imageName = "wooshTrail";
     let wooshImage = Images.getImage(imageName);
-    let trailMaxLength = 10;
+    let trailMaxLength = 12;
+
+    const CURVY_MODE = false; // chop into many small lines? if false, just one long smooth line
 
     // public funcs
     this.draw = function (newX, newY) {
@@ -33,24 +35,25 @@ function WooshTrail(imageName) {
 
         //console.log("Wooshtrail trailXY.length=" + trailXY.length + " draw at " + newX + "," + newY);
 
-        /*
-        // draws many small lines
-        // not stretched and "chopped" as intended
-        // but this supports "curves"
-        for (let segment = 0, count = trailXY.length - 1; segment < count; segment++) {
+        if (CURVY_MODE) {
 
-            drawBitmapLine(wooshImage,
-                trailXY[segment].x, trailXY[segment].y,
-                trailXY[segment + 1].x, trailXY[segment + 1].y);
+            // draws many small lines
+            // not stretched and "chopped" as intended
+            // but this supports "curves"
+            for (let segment = 0, count = trailXY.length - 1; segment < count; segment++) {
 
+                drawBitmapLine(wooshImage,
+                    trailXY[segment].x - 1, trailXY[segment].y,
+                    trailXY[segment + 1].x + 1, trailXY[segment + 1].y);
+
+            }
         }
-        */
+        else {// draw one solid line
+            // draws a line from oldest to newest with no regard to curvature
+            drawBitmapLine(wooshImage,
+                trailXY[0].x, trailXY[0].y,
+                trailXY[trailXY.length - 1].x, trailXY[trailXY.length - 1].y);
 
-        // draws a line from oldest to newest with no regard to curvature
-        drawBitmapLine(wooshImage,
-            trailXY[0].x, trailXY[0].y,
-            trailXY[trailXY.length - 1].x, trailXY[trailXY.length - 1].y);
-
-    }
-
-}
+        } // curvy
+    } // draw 
+} // class
--- js/Player.js
+++ js/Player.js
@@ -6,7 +6,7 @@ function Player() {
 
     var char = new Character(100, 75);
 
-    var states = new PlayerStates(char); 
+    var states = new PlayerStates(char);
     char.initMachine(states);
 
     char.trail = new WooshTrail();
