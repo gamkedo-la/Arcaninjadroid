a43ffcb1c5e96a79a0eb37f43c6ed92c977dd6d5
Sdonai <silentstrum@gmail.com>
2018-08-09 22:23:52 -0700

    tigero stunned

--- CodedAssets/AnimationData/Tigerobot/tigerobotStunned.js
+++ CodedAssets/AnimationData/Tigerobot/tigerobotStunned.js
@@ -1,22 +1,51 @@
-var tigerobotStunnedData= { "frames": [
+var tigerobotStunnedData=  { "frames": [
    {
-    "filename": "tigerobotStunned.aseprite",
+    "filename": "arcaninjadroid #Fist.aseprite",
     "frame": { "x": 0, "y": 0, "w": 48, "h": 32 },
+    "rotated": false,
+    "trimmed": false,
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 32, "h": 32 },
+    "sourceSize": { "w": 32, "h": 32 },
+    "duration": 60
+   },
+   {
+    "filename": "arcaninjadroid #Fist.aseprite",
+    "frame": { "x": 48, "y": 0, "w": 48, "h": 32 },
     "rotated": false,
     "trimmed": false,
     "spriteSourceSize": { "x": 0, "y": 0, "w": 48, "h": 32 },
     "sourceSize": { "w": 48, "h": 32 },
-    "duration": 100
+    "duration": 60
+   },
+   {
+    "filename": "arcaninjadroid #Fist.aseprite",
+    "frame": { "x": 96, "y": 0, "w": 48, "h": 32 },
+    "rotated": false,
+    "trimmed": false,
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 48, "h": 32 },
+    "sourceSize": { "w": 48, "h": 32 },
+    "duration": 60
+   },
+   {
+    "filename": "arcaninjadroid #Fist.aseprite",
+    "frame": { "x": 144, "y": 0, "w": 48, "h": 32 },
+    "rotated": false,
+    "trimmed": false,
+    "spriteSourceSize": { "x": 0, "y": 0, "w": 48, "h": 32 },
+    "sourceSize": { "w": 48, "h": 32 },
+    "duration": 60
    }
  ],
  "meta": {
   "app": "http://www.aseprite.org/",
-  "version": "1.2.9-x64",
-  "image": "C:\\Users\\Remy\\Documents\\Gamkedo\\Arcaninjadroid\\images\\Characters\\Tigerobot\\tigerobotStunned.png",
+  "version": "1.2.8-x64",
+  "image": "C:\\Users\\Remy\\Documents\\Gamkedo\\Arcaninjadroid\\images\\Characters\\Player\\playerIdle.png",
   "format": "RGBA8888",
   "size": { "w": 48, "h": 32 },
   "scale": "1",
   "slices": [
+    { "name": "Hurt", "color": "#6acd5bff", "data": "2", "keys": [{ "frame": 0, "bounds": {"x": 9, "y": 0, "w": 16, "h": 32 } }] }
   ]
  }
 }
+
